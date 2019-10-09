ce18d266b65c4a057bf6f5684207150e1d0b99a0
Yuuki Kojima <yuuki.xx54@gmail.com>
2019-10-10 01:01:50 +0900

    add twitter button component

---
+++ templates/components/organisms/TwitterButtons.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="section">
+    <a :href="href" target="_blank">
+      <v-btn dark color="#00b0ed">Tweet</v-btn>
+    </a>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class DefaultLayout extends Vue {
+  href = ''
+  mounted() {
+    const shareUrl = `https://bananana.io${this.$route.path}`
+    this.href = `https://twitter.com/intent/tweet?url=${shareUrl}&hashtags=bananana`
+  }
+}
+</script>
+
+<style scoped>
+section {
+  text-align: center;
+}
+</style>
